937b51baa1df2e0a8e02203d664cb2a31e24654f
technoid99 <59440183+technoid99@users.noreply.github.com>
2021-03-17 03:38:29 +1100

    enclosed file with <html><body> tags

--- easysplit.py
+++ easysplit.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 
 #Opens the file html_file_to_split.html
-soup = BeautifulSoup(open("html_file_to_split.html"))
+soup = BeautifulSoup(open("test.html"))
 
 #splits it at the search string <div class="content block"> in the HTML file
 #adds <div class="content block"> back in and saves that block as a single html file in pages/
@@ -9,6 +9,6 @@ soup = BeautifulSoup(open("html_file_to_split.html"))
 pageNum = 0
 for i in soup.prettify().split('<div class="content block">'):
     f = open('pages/report' + str(pageNum) + '.html', 'w')        
-    f.write('<div class="content block">'+ ''.join(i))
+    f.write('<html><body><div class="content block">' + ''.join(i) + '</body></html>')
     f.close()
     pageNum = pageNum + 1
