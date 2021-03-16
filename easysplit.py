99d7bbbaad8ae57118c19896b7f5d2558791c3ed
technoid99 <59440183+technoid99@users.noreply.github.com>
2021-03-16 22:33:40 +1100

    Create easysplit.py

---
+++ easysplit.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+
+#Opens the file html_file_to_split.html
+soup = BeautifulSoup(open("html_file_to_split.html"))
+
+#splits it at the search string <div class="content block"> in the HTML file
+#adds <div class="content block"> back in and saves that block as a single html file in pages/
+#repeat
+pageNum = 0
+for i in soup.prettify().split('<div class="content block">'):
+    f = open('pages/report' + str(pageNum) + '.html', 'w')        
+    f.write('<div class="content block">'+ ''.join(i))
+    f.close()
+    pageNum = pageNum + 1
